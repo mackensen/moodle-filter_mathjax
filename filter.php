89c090c984368e700fbdc2459eb9267d1a45d1cd
Jonathon Fowler <fowlerj@usq.edu.au>
2011-11-17 12:50:16 +1000

    getting a little fancier

--- filter.php
+++ filter.php
@@ -7,25 +7,39 @@ class filter_mathjax extends moodle_text_filter {
         global $PAGE, $CFG;
         
         if (isset($PAGE)) {
-            if (file_exists($CFG->dirroot . '/filter/mathjax/js/MathJax.js')) {
-                $mathjaxpath = '/filter/mathjax/js/MathJax.js';
+            if (file_exists($CFG->dirroot . '/filter/mathjax/js/MathJax.js') &&
+                file_exists($CFG->dirroot . '/filter/mathjax/js/config') &&
+                file_exists($CFG->dirroot . '/filter/mathjax/js/jax') &&
+                file_exists($CFG->dirroot . '/filter/mathjax/js/fonts') &&
+                file_exists($CFG->dirroot . '/filter/mathjax/js/extensions')) {
+                $mathjaxpath = '/filter/mathjax/js';
             } else if (substr($PAGE->url, 0, 6) === 'https:') {
-                $mathjaxpath = 'https://d3eoax9i5htok0.cloudfront.net/mathjax/latest/MathJax.js';
+                $mathjaxpath = 'https://d3eoax9i5htok0.cloudfront.net/mathjax/latest';
             } else {
-                $mathjaxpath = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js';
+                $mathjaxpath = 'http://cdn.mathjax.org/mathjax/latest';
             }
             
-            $moodleconfig = new moodle_url('/filter/mathjax/moodle-config.js.php');
-            $url = new moodle_url($mathjaxpath, array('config' =>
-                    'TeX-AMS-MML_HTMLorMML,' . $moodleconfig));
+            $url = new moodle_url($mathjaxpath . '/MathJax.js',
+                  array('config' => 'TeX-AMS-MML_HTMLorMML',
+                        'delayStartupUntil' => 'configured'));
             
             $PAGE->requires->js($url);
+            $PAGE->requires->js_init_call('M.filter_mathjax.init',
+                array('mathjaxroot' => (string)(new moodle_url($mathjaxpath))));
         }
         
         parent::__construct($context, $localconfig);
     }
     
     public function filter($text, array $options = array()) {
+        // The presence of these indicates MathJax ought to process the block:
+        //   inline: \( ... \)
+        //   block: $$ ... $$, \[ ... \]
+        //   tex environments: \begin{...} ... \end{...}
+        
+        if (preg_match('/\$\$.+?\$\$|\\\\\\[.+?\\\\\\]|\\\\\\(.+?\\\\\\)|\\\\begin\\{/s', $text)) {
+            return '<div class="filter-mathjax">'.$text.'</div>';
+        }
         return $text;
     }
 }
