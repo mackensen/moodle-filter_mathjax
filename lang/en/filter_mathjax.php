99cbf3c768b13159f65fb776625dc01a93a624ad
Jonathon Fowler <fowlerj@usq.edu.au>
2012-11-08 09:19:36 +1000

    fixed settings labels, slight mod to the initialisation js

--- lang/en/filter_mathjax.php
+++ lang/en/filter_mathjax.php
@@ -2,5 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['filtername'] = 'Display maths using MathJax';
-$string['distroothttp'] = 'URL (HTTP) to a preferred MathJax distribution. When left empty, the MathJax.org CDN will be used.';
-$string['distroothttps'] = 'URL (HTTPS) to a preferred MathJax distribution. When left empty, the MathJax.org CDN will be used.';
+$string['distroothttp'] = 'MathJax base URL (http)';
+$string['distroothttp_descr'] = 'HTTP URL to a preferred MathJax distribution. When left empty, the MathJax.org CDN will be used.';
+$string['distroothttps'] = 'MathJax base URL (https)';
+$string['distroothttps_descr'] = 'HTTPS URL to a preferred MathJax distribution. When left empty, the MathJax.org CDN will be used.';
